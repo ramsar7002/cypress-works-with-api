56806d6a803feda700b1db457c0bbd59ac7be71d
Ram Sarfian <ramsar7002@gmail.com>
2022-11-13 17:12:31 +0200

    support reporting

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,20 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    configFile: "reporter-config.json",
+  },
+
   env: {
     userName: "artem.bondar16@gmail.com",
     password: "CypressTest1",
     apiUrl: "https://api.realworld.io/api",
+  },
+  retries: {
+    runMode: 1,
+    openMode: 0,
   },
 
   e2e: {
--- cypress/e2e/secondTest.spec.js
+++ cypress/e2e/secondTest.spec.js
@@ -3,7 +3,7 @@ describe('Test log out', () => {
         cy.loginToApplication();
     });
 
-    it('Verify user can log out successfully', () => {
+    it('Verify user can log out successfully', { retries: 1 }, () => {
         cy.contains('.nav-item', 'Settings').click();
         cy.contains('button', ' Or click here to logout. ').click();
         cy.get('.navbar-nav').should('contain', 'Sign up');
